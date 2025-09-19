6fe47abf3a26374ad8ccc64cf105bd6a323c896d
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-09-19 21:42:26 +0100

    Oh my word, the package import thing worked first time

---
+++ cmd/sha256/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/zakkbob/sha256"
+
+func main() {
+	sha256.PackageImportTest()
+}
---
+++ sha256.go
@@ -0,0 +1,7 @@
+package sha256
+
+import "fmt"
+
+func PackageImportTest() {
+	fmt.Print("Oh my word!, it worked")
+}
